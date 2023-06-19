b5e97ce7cec6b8ade4065b70d53c3010cf4e56aa
SaktiDw <sakti596@gmail.com>
2023-06-19 20:00:10 +0800

    add session providers and signout button

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,11 +1,23 @@
+"use client";
 import { getServerSession } from "next-auth";
+import { signOut, useSession } from "next-auth/react";
 import { FC } from "react";
 
 interface pageProps {}
 
-const page: FC<pageProps> = async ({}) => {
-  const session = await getServerSession();
-  return <div>Hallo, {session?.user.email}</div>;
+const page: FC<pageProps> = ({}) => {
+  const { data: session } = useSession();
+  return (
+    <main>
+      <div>Hallo, {session?.user.email}</div>
+      <button
+        className='bg-red-500 rounded-lg px-4 py-2 text-white'
+        onClick={() => signOut()}
+      >
+        Sign Out
+      </button>
+    </main>
+  );
 };
 
 export default page;
