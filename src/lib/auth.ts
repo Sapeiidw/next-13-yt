4f23b3f10fb34cc1ddf56dbea6bc33b896fcdf30
SaktiDw <sakti596@gmail.com>
2023-06-20 17:21:33 +0800

    put accesstoken to session

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,4 @@
-import { NextAuthOptions } from "next-auth";
+import { NextAuthOptions, getServerSession } from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -13,12 +13,15 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
-    async jwt({ token, user }) {
+    async jwt({ token, user, account }) {
       const dbUser = await db.user.findFirst({
         where: { email: token.email },
+        include: { accounts: true },
       });
+
       if (dbUser) {
         token.id = dbUser.id;
+        token.accessToken = dbUser.accounts[0].access_token;
         return token;
       }
 
@@ -27,6 +30,7 @@ export const authOptions: NextAuthOptions = {
         name: dbUser!.name,
         email: dbUser!.email,
         picture: dbUser!.image,
+        accessToken: dbUser!.accounts[0].access_token,
       };
     },
     async session({ session, token }) {
@@ -35,6 +39,7 @@ export const authOptions: NextAuthOptions = {
         session.user.name = token.name;
         session.user.email = token.email;
         session.user.image = token.picture;
+        session.user.accessToken = token.accessToken;
       }
       return session;
     },
@@ -44,3 +49,5 @@ export const authOptions: NextAuthOptions = {
     strategy: "jwt",
   },
 };
+
+export const getAuthSession = () => getServerSession(authOptions);
--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -14,6 +14,7 @@ declare module "next-auth" {
   interface Session {
     user: User & {
       id: UserId;
+      accessToken: string | any;
     };
   }
 }
